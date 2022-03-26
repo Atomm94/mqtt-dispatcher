4300008b58b63dfc1956eb74249758a66be7e3d5
s.aharonyan <s.aharonyan@studio-one.am>
2022-03-26 16:13:27 +0400

    fixed

--- src/app/mqtt/ParseAcu.ts
+++ src/app/mqtt/ParseAcu.ts
@@ -672,10 +672,12 @@ function handleRdUpdateCallback (send_topic: any, crud_message: ICrudMqttMessagi
                 messageAck.send_data = crud_message
                 messageAck.device_topic = topicAck
 
-                if (!crud_message.data.answer_qty) crud_message.data.answer_qty = 0
-                crud_message.data.readers[crud_message.data.answer_qty].messageAck = cloneDeep(messageAck)
-                MQTTBroker.publishMessage(SendTopics.MQTT_CRUD, JSON.stringify(messageAck))
-                crud_message.data.answer_qty++
+                if (!crud_message.data.elevator_mode) {
+                    if (!crud_message.data.answer_qty) crud_message.data.answer_qty = 0
+                    crud_message.data.readers[crud_message.data.answer_qty].messageAck = cloneDeep(messageAck)
+                    MQTTBroker.publishMessage(SendTopics.MQTT_CRUD, JSON.stringify(messageAck))
+                    crud_message.data.answer_qty++
+                }
 
                 if (crud_message.data.elevator_mode) {
                     const acu_reader = cloneDeep(crud_message.data.reader)
