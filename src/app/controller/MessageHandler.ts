06eae64ce9017abad6f209d19941628a24e8bd29
v.martirosyan <v.martirosyan@studioone.am>
2022-09-14 09:59:30 +0400

    add try catch for event

--- src/app/controller/MessageHandler.ts
+++ src/app/controller/MessageHandler.ts
@@ -12,30 +12,34 @@ export default class MessageHandler {
         MQTTBroker.getMessage((topic: string, message: string) => {
             // console.log('getMessage topic', topic, message)
             var data
-            switch (topic) {
-                case 'PING':
-                    // console.log(topic, '-', message)
-                    break
-                case ReceiveTopics.CRUD_MQTT:
-                    data = JSON.parse(message)
-                    if (data.topic === SendTopics.MQTT_SOCKET) {
-                        sendSocket(data)
-                    } else {
-                        ParseCrud.crudData(topic, data)
-                    }
-                    break
-                case ReceiveTopics.LOG:
-                    data = JSON.parse(message)
-                    LogMessageHandler.parseLog(data)
-                    break
-                case SendTopics.MQTT_CRUD:
-                case SendTopics.MQTT_LOG:
-                case SendTopics.MQTT_SOCKET:
-                    break
-                default:
-                    data = JSON.parse(message)
-                    ParseDevice.deviceData(topic, data)
-                    break
+            try {
+                switch (topic) {
+                    case 'PING':
+                        // console.log(topic, '-', message)
+                        break
+                    case ReceiveTopics.CRUD_MQTT:
+                        data = JSON.parse(message)
+                        if (data.topic === SendTopics.MQTT_SOCKET) {
+                            sendSocket(data)
+                        } else {
+                            ParseCrud.crudData(topic, data)
+                        }
+                        break
+                    case ReceiveTopics.LOG:
+                        data = JSON.parse(message)
+                        LogMessageHandler.parseLog(data)
+                        break
+                    case SendTopics.MQTT_CRUD:
+                    case SendTopics.MQTT_LOG:
+                    case SendTopics.MQTT_SOCKET:
+                        break
+                    default:
+                        data = JSON.parse(message)
+                        ParseDevice.deviceData(topic, data)
+                        break
+                }
+            } catch (error) {
+                console.log(444, error)
             }
         })
     }
